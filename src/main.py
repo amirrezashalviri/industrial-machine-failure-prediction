3cc7f165e2e7d8b236d90f0b4aa7ad30ab7f2093
Amirreza Shalviri <amirrezashalviri@gmail.com>
2026-08-19 13:07:57 +0330

    Analysis Data.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 DATA_PATH = "data/raw/ai4i2020.csv"
@@ -12,20 +13,45 @@ def load_data(path: str) -> pd.DataFrame:
 def main():
     data = load_data(DATA_PATH)
 
-    print("Dataset shape:")
-    print(data.shape)
+    print("\n****************************************************************************************************")
+    print("\nDataset Info:")
+    print(data.info())
 
-    print("\nDataset columns:")
-    print(data.columns.tolist())
-
-    print("\nMissing values:")
-    print(data.isnull().sum())
-
-    print("\nMachine failure distribution:")
+    print("\n****************************************************************************************************")
+    print("\nGeneral Machine failure Info:")
     print(data["Machine failure"].value_counts())
 
-    print("\nNumerical feature statistics:")
-    print(data.describe())
+    features = [
+        "Air temperature [K]",
+        "Process temperature [K]",
+        "Rotational speed [rpm]",
+        "Torque [Nm]",
+        "Tool wear [min]",
+    ]
+
+    print("\n****************************************************************************************************")
+    print("\nMachine failure Info By Selected Cols:")
+    print(data.groupby("Machine failure")[features].mean())
+
+    print("\n****************************************************************************************************")
+    print("\nTool Wear Distribution by Machine Failure:")
+
+    #failure_0 = data[data["Machine failure"] == 0]["Tool wear [min]"]
+    #failure_1 = data[data["Machine failure"] == 1]["Tool wear [min]"]
+
+    failure_0 = data[data["Machine failure"] == 0]["Torque [Nm]"]
+    failure_1 = data[data["Machine failure"] == 1]["Torque [Nm]"]
+
+    plt.hist(failure_0, bins=30, alpha=0.6, label="No Failure", density=True)
+    plt.hist(failure_1, bins=30, alpha=0.6, label="Failure", density=True)
+
+    plt.xlabel("Tool wear [min]")
+    plt.ylabel("Number of machines")
+    plt.title("Tool Wear Distribution by Machine Failure")
+    plt.legend()
+
+    #plt.show()
+    plt.savefig("data/tool_wear_distribution.png")
 
 
 if __name__ == "__main__":
